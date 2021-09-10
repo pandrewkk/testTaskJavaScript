3f565179bf7a2bab85f33ddf69f46cce0c254f2c
HUCKSTERinc <pandrew.kk@gmail.com>
2021-09-10 12:20:17 +0400

    added id

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-const arr = [{author: 'Достоевский Ф. М.', title: 'Игрок', genre: 'Роман'}, {author: 'Смолин А. П.', title: 'Ремонт эксковатора Э-505', genre: 'Пособие'}, {author: 'Харари Ю.', title: 'Sapiens. Краткая история человечества', genre: 'Научпоп'}];
+const arr = [{id: 1, author: 'Достоевский Ф. М.', title: 'Игрок', genre: 'Роман'}, {id: 2, author: 'Смолин А. П.', title: 'Ремонт эксковатора Э-505', genre: 'Пособие'}, {id: 3, author: 'Харари Ю.', title: 'Sapiens. Краткая история человечества', genre: 'Научпоп'}];
 function showBooks(arr) {
     const tableBody = document.querySelector('.tbody');
     let tableRow = '';
-    arr.forEach((book, index) => {
-        tableRow += `<tr><td>${index + 1}</td><td>${book.author}</td><td>${book.title}</td><td>${book.genre}</td></tr>`;
+    arr.forEach((book) => {
+        tableRow += `<tr><td>${book.id}</td><td>${book.author}</td><td>${book.title}</td><td>${book.genre}</td></tr>`;
     })
     tableBody.innerHTML = tableRow;
 }

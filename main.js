0ef9c528626623e53c7b1df1d7e243870706e906
HUCKSTERinc <pandrew.kk@gmail.com>
2021-09-10 16:56:44 +0400

    with createElement

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
+const books = [
+    {id: 1, author: 'Достоевский Ф. М.', title: 'Игрок', genre: 'Роман'},
+    {id: 2, author: 'Смолин А. П.', title: 'Ремонт эксковатора Э-505', genre: 'Пособие'},
+    {id: 3, author: 'Харари Ю.', title: 'Sapiens. Краткая история человечества', genre: 'Научпоп'}
+];
 
-const arr = [{id: 1, author: 'Достоевский Ф. М.', title: 'Игрок', genre: 'Роман'}, {id: 2, author: 'Смолин А. П.', title: 'Ремонт эксковатора Э-505', genre: 'Пособие'}, {id: 3, author: 'Харари Ю.', title: 'Sapiens. Краткая история человечества', genre: 'Научпоп'}];
-function showBooks(arr) {
-    const tableBody = document.querySelector('.tbody');
-    let tableRow = '';
-    arr.forEach((book) => {
-        tableRow += `<tr><td>${book.id}</td><td>${book.author}</td><td>${book.title}</td><td>${book.genre}</td></tr>`;
-    })
-    tableBody.innerHTML = tableRow;
+const table = document.querySelector("table");
+
+function showBooks(table, books) {
+    for (let element of books) {
+        let row = table.insertRow();
+        for (let key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
 }
 
-showBooks(arr);
+showBooks(table, books);

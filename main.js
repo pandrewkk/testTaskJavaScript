35bfb3d36b0ce4f8dc93028b05818510bc0a7b0e
HUCKSTERinc <pandrew.kk@gmail.com>
2021-09-29 09:53:11 +0400

    sortWorks

--- main.js
+++ main.js
@@ -96,7 +96,6 @@ function sortTableByColumn(table, column, asc = true) {
     const tBody = table.tBodies[0];
     const rows = Array.from(tBody.querySelectorAll("tr"));
 
-    // Sort each row
     const sortedRows = rows.sort((a, b) => {
         const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
         const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
@@ -104,15 +103,13 @@ function sortTableByColumn(table, column, asc = true) {
         return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
     });
 
-    // Remove all existing TRs from the table
     while (tBody.firstChild) {
         tBody.removeChild(tBody.firstChild);
     }
 
-    // Re-add the newly sorted rows
+
     tBody.append(...sortedRows);
 
-    // Remember how the column is currently sorted
     table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
     table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
     table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
